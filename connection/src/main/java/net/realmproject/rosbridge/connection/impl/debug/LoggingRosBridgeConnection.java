63007325a4eab777898816a1e9d81c7254ddf67a
Nathaniel Sherry <nsherry4@uwo.ca>
2015-01-19 14:31:19 -0500

    Added constructor to LoggingRosBridgeConnection to use a default PrintWriter to system out

--- connection/src/main/java/net/realmproject/rosbridge/connection/impl/debug/LoggingRosBridgeConnection.java
+++ connection/src/main/java/net/realmproject/rosbridge/connection/impl/debug/LoggingRosBridgeConnection.java
@@ -1,84 +1,88 @@
 package net.realmproject.rosbridge.connection.impl.debug;
 
-import java.io.IOException;
-import java.io.Writer;
 
-import javax.websocket.DeploymentException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 import net.realmproject.rosbridge.connection.RosBridgeConnection;
 import net.realmproject.rosbridge.connection.RosBridgeMessage;
 import net.realmproject.rosbridge.connection.RosBridgeMessageListener;
 import net.realmproject.rosbridge.util.RosBridgeSerialize;
 
+
 public class LoggingRosBridgeConnection implements RosBridgeConnection {
 
-	private RosBridgeConnection backer;
-	private Writer log;
-	private boolean logInput = true;
-	private boolean logOutput = true;
-	
-	public LoggingRosBridgeConnection(RosBridgeConnection backer, final Writer log) {
-		this(backer, log, true, true);
-	}
-	
-	public LoggingRosBridgeConnection(RosBridgeConnection backer, final Writer log, final boolean logInput, final boolean logOutput) {
-		this.backer = backer;
-		this.log = log;
-		this.logInput = logInput;
-		this.logOutput = logOutput;
-		
-		backer.addMessageListener(message -> {
-			try {
-				if (logInput) { 
-					log.write("Receiving: " + RosBridgeSerialize.serialize(message) + "\n");
-					log.flush();
-				}
-			}
-			catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		});
-		
-	}
-	
-	
-	public void sendMessage(String message) throws InterruptedException, IOException {
-		try {
-			if (logOutput) { 
-				log.write("Sending: " + message + "\n");
-				log.flush();
-			}
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		backer.sendMessage(message);
-	}
+    private RosBridgeConnection backer;
+    private Writer log;
+    private boolean logInput = true;
+    private boolean logOutput = true;
 
-	public void addMessageListener(RosBridgeMessageListener listener) {
-		backer.addMessageListener(listener);
-	}
+    public LoggingRosBridgeConnection(RosBridgeConnection backer) {
+        this(backer, new PrintWriter(System.out));
+    }
 
-	public void clearMessageListeners() {
-		backer.clearMessageListeners();
-	}
+    public LoggingRosBridgeConnection(RosBridgeConnection backer, final Writer log) {
+        this(backer, log, true, true);
+    }
 
-	public void close() throws IOException {
-		backer.close();
-	}
+    public LoggingRosBridgeConnection(RosBridgeConnection backer, final Writer log, final boolean logInput,
+            final boolean logOutput) {
+        this.backer = backer;
+        this.log = log;
+        this.logInput = logInput;
+        this.logOutput = logOutput;
 
-	public String getNewMessageID() {
-		return backer.getNewMessageID();
-	}
+        backer.addMessageListener(message -> {
+            try {
+                if (logInput) {
+                    log.write("Receiving: " + RosBridgeSerialize.serialize(message) + "\n");
+                    log.flush();
+                }
+            }
+            catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
 
-	public void removeMessageListener(RosBridgeMessageListener listener) {
-		backer.removeMessageListener(listener);
-	}
+    }
 
-	public void notifyMessageListeners(RosBridgeMessage message) {
-		backer.notifyMessageListeners(message);
-	}
+    public void sendMessage(String message) throws InterruptedException, IOException {
+        try {
+            if (logOutput) {
+                log.write("Sending: " + message + "\n");
+                log.flush();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        backer.sendMessage(message);
+    }
 
-	
+    public void addMessageListener(RosBridgeMessageListener listener) {
+        backer.addMessageListener(listener);
+    }
+
+    public void clearMessageListeners() {
+        backer.clearMessageListeners();
+    }
+
+    public void close() throws IOException {
+        backer.close();
+    }
+
+    public String getNewMessageID() {
+        return backer.getNewMessageID();
+    }
+
+    public void removeMessageListener(RosBridgeMessageListener listener) {
+        backer.removeMessageListener(listener);
+    }
+
+    public void notifyMessageListeners(RosBridgeMessage message) {
+        backer.notifyMessageListeners(message);
+    }
+
 }
